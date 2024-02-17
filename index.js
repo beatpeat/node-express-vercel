a9ba1f0399e73924521eb7bd2f444d0c36fbebb5
beatpeat <129691589+beatpeat@users.noreply.github.com>
2024-02-17 13:04:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // Import packages
 const express = require("express");
-const home = require("./routes/home");
+const router = require("router");
 
+module.exports = router;
 // Middlewares
 const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/home", home);
+app.use("/home", router.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Express Testing 2",
+    message: "The app is working properly!",
+  });
+}));
 
 // connection
 const port = process.env.PORT || 9001;

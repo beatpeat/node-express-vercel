208a2b1cf5691911b745261b501b0f570481d703
beatpeat <129691589+beatpeat@users.noreply.github.com>
2024-02-17 13:12:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,21 @@ const express = require("express");
 // Create router
 const router = express.Router();
 
+// Define route
+router.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
+});
+
 // Middlewares
 const app = express();
 app.use(express.json());
 
 // Routes
-router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing 2",
-    message: "The app is working properly!",
-  });
-});
-
-// Use router middleware
 app.use("/home", router);
 
 // Connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
-
-// Export router
-module.exports = router;
